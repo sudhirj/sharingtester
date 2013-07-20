4f43befcab3cc049dee4c5c42b8b70bf0dc97147
Sudhir Jonathan <sudhir.j@gmail.com>
2013-07-20 11:51:44 +0530

    App Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sharingtester::Application.config.secret_key_base = 'e62bbf761e2f1e358383f92b159220baad38c2f36b95f4366692a3e249a8a40e29b704c6d4ff4242791d9eebe11fd31a98b5b0f183a4fbd17fc5647a29c2a3d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sharingtester::Application.config.session_store :cookie_store, key: '_sharingtester_session'
