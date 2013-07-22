0ba979df7338da0dcb4ee36c1d200bd18d275464
Sudhir Jonathan <sudhir.j@gmail.com>
2013-07-23 01:16:09 +0530

    Added search engines

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 	end
 
 	def analyze
-		radar_endpoint = 'http://r7-radar.herokuapp.com/v1/basic'
+		radar_endpoint = 'http://r7-radar.herokuapp.com/v1/full'
 		response = HTTParty.get "#{radar_endpoint}?url=#{URI::escape params[:url]}"
 		@analysis = Hashie::Mash.new JSON.parse response.body
 		render 'analysis', layout: nil
